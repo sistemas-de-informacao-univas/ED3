1a4ec538864a13443e7d6f0a5dc8cb1e8aa99165
Cristiano Vieira <47859813+profcristianovs@users.noreply.github.com>
2020-03-16 15:33:23 -0300

    Create exemplo josephus (com array).java

---
+++ exemplo josephus (com array).java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Josephus {
+    public static void main(String[] argv) 
+    {
+        List<Integer> r;
+        System.out.println(r = josephus(10, 3, 1));                    
+        System.out.printf("Person %d is last\n", r.get(r.size() - 1));  
+    }
+
+    static List<Integer> josephus(int N, int step, int start)
+    {
+        if (N < 1 || step < 1 || start < 1) return null;
+
+        List<Integer> p = new LinkedList<Integer>();
+        for (int i = 0; i < N; i++)
+            p.add(i+1);
+
+        List<Integer> r = new LinkedList<Integer>();
+        int i = (start - 2) % N;
+        for (int j = N; j > 0; j--) {
+            i = (i + step) % N--;
+            r.add(p.remove(i--));
+        }
+
+        return r;
+    }
+}
