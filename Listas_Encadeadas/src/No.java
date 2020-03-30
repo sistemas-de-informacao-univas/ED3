d0f66623edf66cec514884e94092c69735f70009
Cristiano Vieira <47859813+profcristianovs@users.noreply.github.com>
2020-03-30 14:41:39 -0300

    Add files via upload

---
+++ Listas_Encadeadas/src/No.java
@@ -0,0 +1,23 @@
+
+public class No {
+
+	private int elemento;
+	private No proximo;
+	
+	public void setElemento(int elemento) {
+		this.elemento= elemento;
+	}
+	
+	public int getElemento() {
+		return this.elemento;
+	}
+	
+	public void setProximo(No proximo) {
+		this.proximo = proximo;
+	}
+	
+	public No getProximo() {
+		return this.proximo;
+	}
+	
+}
---
+++ Listas_Encadeadas/src/Principal.java
@@ -0,0 +1,17 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Lista lista = new Lista();
+		
+		lista.inserir(1);
+		lista.inserir(2);
+		lista.inserir(3);
+		lista.inserir(4);
+		lista.inserir(5);
+		
+		lista.listar();
+
+	}
+
+}
