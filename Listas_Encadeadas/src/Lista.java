6c118e8246432e4802b3810891f376c3b384fa4c
Cristiano Vieira <47859813+profcristianovs@users.noreply.github.com>
2020-06-23 21:57:37 -0300

    Update Lista.java

--- Listas_Encadeadas/src/Lista.java
+++ Listas_Encadeadas/src/Lista.java
@@ -27,7 +27,7 @@ public class Lista {
 		
 	public void listar() {
 		if(inicio == null) {
-			System.out.println("N�o h� elementos na lista");
+			System.out.println("Não há elementos na lista");
 		}
 		else {
 			No aux = inicio;
@@ -42,4 +42,59 @@ public class Lista {
 	public boolean vazio() {
 		return this.inicio == null;
 	}
+	
+	public void remove(int elemento)
+	{
+		No aux;
+		aux=Busca(elemento);
+		if (aux!=null){ // se existe o No a ser deletado
+			
+				if (aux==inicio)// se o Nó esta no começo
+				{
+					inicio=aux.getProximo();
+					aux.getProximo().setAnterior(null);
+					System.out.println("Removendo do inicio: " + elemento);
+				}else {
+					if (aux==fim)// se o Nó esta no fim
+					{
+						fim=aux.getAnterior();
+						aux.getAnterior().setProximo(null);
+						System.out.println("Removendo do fim: " + elemento);
+					}else{ // se o no esta no meio
+						aux.getAnterior().setProximo(aux.getProximo());
+						aux.getProximo().setAnterior(aux.getAnterior());
+						System.out.println("Removendo do meio: " + elemento);
+					}
+				}
+				aux.setAnterior(null);
+				aux.setProximo(null);
+				System.out.println("Elemento removido: " + elemento);
+			
+		}else {
+			System.out.println("Elemento nao encontrado");
+		}
+	}
+	
+	
+	
+	
+	public boolean buscar(int valor) throws Exception{
+		
+		
+		int posicaoLista = pri_ocup;
+		
+		for(int i = 0; i< qtde_itens; i++){
+			if(lista_dados[posicaoLista] == valor){
+				return true;
+			
+			}else{
+				posicaoLista = array_prox[posicaoLista];
+			}
+		}
+		return false;
+	}
+	
+	
+	
+	
 }
